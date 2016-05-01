f928367e45515f11adfd2fc3426e984498d0fb7b
admin <admin@admindeMBP.home>
2016-04-30 21:36:36 -0400

    update1

--- main.go
+++ main.go
@@ -3,20 +3,42 @@ package main
 import (
   "fmt"
 )
+
 func main() {
-  var plantCapacities []float64
+  plantCapacities := []float64{30,30,30,60,60,100}
 
-  plantCapacities = []float64{30,30,30,60,60,100}
+  activePlants := []int{0,1}
+  gridLoad := 75.
 
-  var capacity float64 = plantCapacities[0] + plantCapacities[1] +
-  plantCapacities[2] + plantCapacities[3] +plantCapacities[4] +
-  plantCapacities[5]
+  fmt.Println("1) Generate Power Plant Report")
+  fmt.Println("2) Generate Power Grid Report")
+  fmt.Print("Please choose an option: ")
 
-  var gridLoad = 75.
+  var option string
+  fmt.Scanln(&option)
 
-  utilization := gridLoad / capacity
+  switch option {
+  case "1":
+    generatePlantCapacityReport(plantCapacities...)
+  case "2":
+    generatePowerGridReport(activePlants, plantCapacities, gridLoad)
+  default:
+    fmt.Printf("unkown option\n")
+  }
+}
 
-  fmt.Printf("%-20s%.0f\n","Capacity: ", capacity)
-  fmt.Printf("%-20s%.0f\n","Load: ", gridLoad)
-  fmt.Printf("%-20s%.1f%%\n","Utilization: ", utilization*100)
+func generatePlantCapacityReport(plantCapacities ...float64) {
+  for idx, cap := range plantCapacities {
+    fmt.Printf("Plant: %d Capacity: %.0f\n", idx, cap)
+  }
+}
+
+func generatePowerGridReport(activePlants []int, plantCapacities []float64, gridLoad float64) {
+  capacity := 0.
+  for _, plantId := range activePlants {
+    capacity += plantCapacities[plantId]
+  }
+  fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+  fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+  fmt.Printf("%-20s%.1f\n", "Utilization: ", gridLoad/capacity*100)
 }
